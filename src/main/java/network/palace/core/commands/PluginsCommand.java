619c9470ec535c19b3760fe3c002f185065d7700
Cameron Weaver (Clutch) <lockerzmodding@gmail.com>
2017-01-09 14:29:48 -0500

    Replace help commands with plugins command

--- src/main/java/network/palace/core/commands/PluginsCommand.java
+++ src/main/java/network/palace/core/commands/PluginsCommand.java
@@ -24,7 +24,7 @@ import java.net.URLEncoder;
 import java.util.*;
 import java.util.concurrent.locks.ReentrantLock;
 
-@CommandMeta(aliases = {"about", "pl", "ver", "version"}, description = "Lists the plugins for the server.")
+@CommandMeta(aliases = {"about", "pl", "ver", "version", "help", "?"}, description = "Lists the plugins for the server.")
 @CommandPermission(rank = Rank.WIZARD)
 public class PluginsCommand extends CoreCommand {
 
