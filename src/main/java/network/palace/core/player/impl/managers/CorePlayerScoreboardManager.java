ff8463eb6ee811c142e208db3189bb3ac68bc197
Cameron Weaver (Clutch) <lockerzmodding@gmail.com>
2017-01-21 21:59:01 -0500

    Fix too long objective name for scoreboard

--- src/main/java/network/palace/core/player/impl/managers/CorePlayerScoreboardManager.java
+++ src/main/java/network/palace/core/player/impl/managers/CorePlayerScoreboardManager.java
@@ -86,7 +86,7 @@ public class CorePlayerScoreboardManager implements CPlayerScoreboardManager {
         if (scoreboard == null) return;
         if (player.getBukkitPlayer() == null) return;
         player.getBukkitPlayer().setScoreboard(scoreboard);
-        scoreboardObjective = scoreboard.registerNewObjective("obj" + player.getName(), "dummy");
+        scoreboardObjective = scoreboard.registerNewObjective(player.getName(), "dummy");
         scoreboardObjective.setDisplaySlot(DisplaySlot.SIDEBAR);
         isSetup = true;
     }
