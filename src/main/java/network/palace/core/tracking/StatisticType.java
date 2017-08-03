0950b656a1c070130477d99ccc71269218076280
Innectic <innectic@gmail.com>
2017-08-02 21:35:55 -0700

    More statistic types

--- src/main/java/network/palace/core/tracking/StatisticType.java
+++ src/main/java/network/palace/core/tracking/StatisticType.java
@@ -12,18 +12,12 @@ import org.bukkit.Statistic;
 public class StatisticType {
     @Getter private String type;
 
-    /**
-     * Statistics shared between all games
-     */
     public static class Global {
         public static final StatisticType TIME_PLAYED = new StatisticType("time_played");
         public static final StatisticType WINS = new StatisticType("wins");
         public static final StatisticType LOSSES = new StatisticType("losses");
     }
 
-    /**
-     * Statistics about Outpost
-     */
     public static class Outpost {
         public static final StatisticType ELIMINATIONS = new StatisticType("eliminations");
         public static final StatisticType DEATHS = new StatisticType("deaths");
@@ -31,27 +25,23 @@ public class StatisticType {
         public static final StatisticType SECONDS_ON_TOWER = new StatisticType("seconds_on_tower");
     }
 
-    /**
-     * Statistics about Spleef
-     */
     public static class Spleef {
         public static final StatisticType DEATHS = new StatisticType("deaths");
         public static final StatisticType BLOCKS_REMOVED = new StatisticType("blocks_removed");
     }
 
-    /**
-     * Statistics about OneShot
-     */
     public static class OneShot {
         public static final StatisticType ELIMINATIONS = new StatisticType("eliminations");
         public static final StatisticType SHOTS_MISSED = new StatisticType("shots_missed");
     }
 
-    /**
-     * Statistics about FreezeTag
-     */
     public static class FreezeTag {
         public static final StatisticType FROZEN_PLAYERS = new StatisticType("frozen_players");
         public static final StatisticType UNFROZEN_PLAYERS = new StatisticType("unfrozen_players");
+    }
+
+    public static class PIXIE_DUST_SHOOTOUT {
+        public static final StatisticType ELIMINATIONS = new StatisticType("eliminations");
+        public static final StatisticType DEATHS = new StatisticType("deaths");
     }
 }
