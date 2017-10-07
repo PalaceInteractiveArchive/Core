bf91f5ea24caab2653c16c5ca507d84875d59aa3
Legobuilder0813 <legobuilder0813@gmail.com>
2017-10-07 03:25:21 -0400

    Super hacky fix

--- src/main/java/network/palace/core/Core.java
+++ src/main/java/network/palace/core/Core.java
@@ -609,12 +609,13 @@ public class Core extends JavaPlugin {
      * @param callback the callbacks for the actions
      */
     public static void sendAllPlayers(String server, Callback callback) {
-        runTaskAsynchronously(() -> {
-            do {
-                CPlayer player = Core.getPlayerManager().getOnlinePlayers().get(0);
-                player.sendToServer(server);
-            } while (Core.getPlayerManager().getOnlinePlayers().size() > 0);
-            callback.finished();
-        });
+        for (CPlayer player : Core.getPlayerManager().getOnlinePlayers()) {
+            player.sendToServer(server);
+        }
+        Core.runTaskTimer(() -> {
+            if (Core.getPlayerManager().getOnlinePlayers().isEmpty()) {
+                callback.finished();
+            }
+        }, 0L, 40L);
     }
 }
