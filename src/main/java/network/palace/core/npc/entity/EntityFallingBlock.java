af446dc92959fdf1333e4eec6fceb6c83d5f84a6
Marc <legobuilder0813@gmail.com>
2019-06-13 15:16:53 -0400

    Update EntityFallingBlock for 1.13

--- src/main/java/network/palace/core/npc/entity/EntityFallingBlock.java
+++ src/main/java/network/palace/core/npc/entity/EntityFallingBlock.java
@@ -1,24 +1,34 @@
 package network.palace.core.npc.entity;
 
+import com.comphenix.protocol.utility.MinecraftReflection;
 import lombok.Getter;
 import network.palace.core.npc.AbstractEntity;
 import network.palace.core.packets.AbstractPacket;
 import network.palace.core.packets.server.entity.WrapperPlayServerSpawnEntity;
 import network.palace.core.pathfinding.Point;
 import network.palace.core.player.CPlayer;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.EntityType;
 
 import java.util.Set;
 import java.util.UUID;
 
 public class EntityFallingBlock extends AbstractEntity {
-    @Getter private final int typeId;
-    @Getter private final byte data;
+    @Getter private int blockData;
 
-    public EntityFallingBlock(Point location, Set<CPlayer> observers, String title, int typeId, byte data) {
+    public EntityFallingBlock(Point location, Set<CPlayer> observers, String title, BlockData blockData) {
         super(location, observers, title);
-        this.typeId = typeId;
-        this.data = data;
+        try {
+            Object craftMeta = MinecraftReflection.getCraftBukkitClass("CraftBlockData").cast(blockData);
+            Object craftState = craftMeta.getClass().getMethod("getState").invoke(craftMeta);
+
+            Object minecraftBlockClass = MinecraftReflection.getMinecraftClass("Block");
+            Object id = minecraftBlockClass.getClass().getMethod("getCombinedId").invoke(craftState);
+            this.blockData = (int) id;
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.blockData = 0;
+        }
     }
 
     @Override
@@ -35,7 +45,7 @@ public class EntityFallingBlock extends AbstractEntity {
         wrapper.setX(location.getX());
         wrapper.setY(location.getY());
         wrapper.setZ(location.getZ());
-        wrapper.setObjectData(typeId | data << 12);
+        wrapper.setObjectData(blockData);
         return wrapper;
     }
 }
