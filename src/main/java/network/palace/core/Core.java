22c372b2ff33737be97f68b1c83215e6b84c1b75
Marc DiFilippo <legobuilder0813@gmail.com>
2020-03-29 01:37:48 -0400

    Update to v2.5.9

--- src/main/java/network/palace/core/Core.java
+++ src/main/java/network/palace/core/Core.java
@@ -61,7 +61,7 @@ import java.util.concurrent.Future;
  * <p>
  * You can access instances of other modules by depending on Core in your pom.xml, and then executing Core.get
  */
-@PluginInfo(name = "Core", version = "2.5.8", depend = {"ProtocolLib"}, softdepend = {"ViaVersion"})
+@PluginInfo(name = "Core", version = "2.5.9", depend = {"ProtocolLib"}, softdepend = {"ViaVersion"})
 public class Core extends JavaPlugin {
     @Getter private URLClassLoader coreClassLoader;
 
