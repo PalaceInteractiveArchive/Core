eee5df1478c37390e90d49ec91025f515618b16f
Marc <me@marcdif.me>
2020-09-05 06:12:10 -0400

    Add asynchronous loading for inventory menus

--- src/main/java/network/palace/core/menu/Menu.java
+++ src/main/java/network/palace/core/menu/Menu.java
@@ -14,23 +14,46 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class Menu implements Listener {
+    private static final List<Menu> toOpen = new ArrayList<>();
+
+    static {
+        Core.runTaskTimer(Core.getInstance(), () -> {
+            List<Menu> opened = new ArrayList<>();
+            toOpen.stream().filter(Objects::nonNull).forEach((menu) -> {
+                menu.open_internal();
+                opened.add(menu);
+            });
+            toOpen.removeAll(opened);
+        }, 0L, 5L);
+    }
+
     @Getter private final int size;
     private final List<MenuButton> menuButtons;
     private final Inventory inventory;
     private final String title;
     private final CPlayer player;
-    private Optional<Runnable> onClose = Optional.empty();
+    private Runnable onClose = null;
+
+    private final Runnable callback;
+    private boolean hasCallbackRun = false;
 
     public Menu(int size, String title, CPlayer player, List<MenuButton> buttons) {
+        this(size, title, player, buttons, null);
+    }
+
+    public Menu(int size, String title, CPlayer player, List<MenuButton> buttons, Runnable callback) {
         this.size = size;
         this.inventory = Core.createInventory(size, title);
         this.title = title;
         this.player = player;
         this.menuButtons = buttons;
+        this.callback = callback;
     }
 
     public Optional<MenuButton> getButton(int slot) {
@@ -38,18 +61,30 @@ public class Menu implements Listener {
     }
 
     public void open() {
-        Core.runTask(Core.getInstance(), () -> {
-            inventory.clear();
-            menuButtons.forEach(button -> {
-                if (button.getSlot() >= inventory.getSize()) {
-                    throw new IllegalArgumentException("Button " + button.getItemStack().toString() + " in slot " +
-                            button.getSlot() + " exceeds inventory " + title + " size " + inventory.getSize());
-                }
-                inventory.setItem(button.getSlot(), button.getItemStack());
-            });
-            player.openInventory(inventory);
-            Core.registerListener(this);
+        if (!hasCallbackRun) {
+            hasCallbackRun = true;
+            if (this.callback != null) {
+                Core.runTaskAsynchronously(Core.getInstance(), () -> {
+                    callback.run();
+                    toOpen.add(this);
+                });
+                return;
+            }
+            toOpen.add(this);
+        }
+    }
+
+    private void open_internal() {
+        inventory.clear();
+        menuButtons.forEach(button -> {
+            if (button.getSlot() >= inventory.getSize()) {
+                throw new IllegalArgumentException("Button " + button.getItemStack().toString() + " in slot " +
+                        button.getSlot() + " exceeds inventory " + title + " size " + inventory.getSize());
+            }
+            inventory.setItem(button.getSlot(), button.getItemStack());
         });
+        player.openInventory(inventory);
+        Core.registerListener(this);
     }
 
     public void removeButton(int slot) {
@@ -89,14 +124,16 @@ public class Menu implements Listener {
     }
 
     public void setOnClose(Runnable onClose) {
-        this.onClose = Optional.ofNullable(onClose);
+        this.onClose = onClose;
     }
 
     @EventHandler
     public void close(InventoryCloseEvent event) {
         if (isSameInventory(event.getView())) {
             HandlerList.unregisterAll(this);
-            onClose.ifPresent(Runnable::run);
+            if (onClose != null) {
+                onClose.run();
+            }
         }
     }
 
