89818d2de2c96ce4292bcb9a5c4b43b760183384
Tom Roman <tom@tomroman.co.uk>
2021-03-23 20:45:27 +0000

    Okay now its fixed

--- src/main/java/network/palace/core/player/impl/managers/CorePlayerScoreboardManager.java
+++ src/main/java/network/palace/core/player/impl/managers/CorePlayerScoreboardManager.java
@@ -145,7 +145,7 @@ public class CorePlayerScoreboardManager implements CPlayerScoreboardManager {
         if (scoreboard == null) setup();
         for (Rank rank : Rank.values()) {
             if (scoreboard.getTeam(rank.getScoreboardName()) != null) continue;
-            Team team = scoreboard.registerNewTeam(rank.getScoreboardName() + rank.getName());
+            Team team = scoreboard.registerNewTeam(rank.getScoreboardName());
             team.setPrefix(rank.getFormattedName() + " ");
             team.setColor(rank.getTagColor());
             team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
