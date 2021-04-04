7c0915d60ecbd2bfd83084e19613ade0ff5bd741
Marc DiFilippo <marc@marcdif.com>
2021-04-03 22:22:38 -0400

    Fix sending resource packs on 1.13

--- src/main/java/network/palace/core/resource/ResourceListener.java
+++ src/main/java/network/palace/core/resource/ResourceListener.java
@@ -29,32 +29,35 @@ public class ResourceListener extends PacketAdapter {
     public void onPacketReceiving(PacketEvent event) {
         if (event.getPlayer() == null) return;
         CPlayer player = Core.getPlayerManager().getPlayer(event.getPlayer());
-        try {
-            PacketContainer packet = event.getPacket();
-            EnumWrappers.ResourcePackStatus status = packet.getResourcePackStatus().read(0);
-            switch (status) {
-                case SUCCESSFULLY_LOADED:
-                    new ResourceStatusEvent(PackStatus.LOADED, player).call();
-                    Core.getResourceManager().downloadingResult(player.getUniqueId(), PackStatus.LOADED);
-                    return;
-                case DECLINED:
-                    new ResourceStatusEvent(PackStatus.DECLINED, player).call();
-                    Core.getResourceManager().downloadingResult(player.getUniqueId(), PackStatus.DECLINED);
-                    return;
-                case FAILED_DOWNLOAD:
-                    new ResourceStatusEvent(PackStatus.FAILED, player).call();
-                    Core.getResourceManager().downloadingResult(player.getUniqueId(), PackStatus.FAILED);
-                    return;
-                case ACCEPTED:
-                    new ResourceStatusEvent(PackStatus.ACCEPTED, player).call();
-                    return;
-                default:
-                    Core.getResourceManager().downloadingResult(player.getUniqueId(), null);
-                    player.sendMessage(ChatColor.RED + "There seems to be an error, please report this to a Staff Member! (Error Code 100)");
+        if (player == null) return;
+        Core.runTask(Core.getInstance(), () -> {
+            try {
+                PacketContainer packet = event.getPacket();
+                EnumWrappers.ResourcePackStatus status = packet.getResourcePackStatus().read(0);
+                switch (status) {
+                    case SUCCESSFULLY_LOADED:
+                        new ResourceStatusEvent(PackStatus.LOADED, player).call();
+                        Core.getResourceManager().downloadingResult(player.getUniqueId(), PackStatus.LOADED);
+                        return;
+                    case DECLINED:
+                        new ResourceStatusEvent(PackStatus.DECLINED, player).call();
+                        Core.getResourceManager().downloadingResult(player.getUniqueId(), PackStatus.DECLINED);
+                        return;
+                    case FAILED_DOWNLOAD:
+                        new ResourceStatusEvent(PackStatus.FAILED, player).call();
+                        Core.getResourceManager().downloadingResult(player.getUniqueId(), PackStatus.FAILED);
+                        return;
+                    case ACCEPTED:
+                        new ResourceStatusEvent(PackStatus.ACCEPTED, player).call();
+                        return;
+                    default:
+                        Core.getResourceManager().downloadingResult(player.getUniqueId(), null);
+                        player.sendMessage(ChatColor.RED + "There seems to be an error, please report this to a Staff Member! (Error Code 100)");
+                }
+            } catch (Exception e) {
+                player.sendMessage(ChatColor.RED + "There seems to be an error, please report this to a Staff Member! (Error Code 101)");
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            player.sendMessage(ChatColor.RED + "There seems to be an error, please report this to a Staff Member! (Error Code 101)");
-            e.printStackTrace();
-        }
+        });
     }
 }
