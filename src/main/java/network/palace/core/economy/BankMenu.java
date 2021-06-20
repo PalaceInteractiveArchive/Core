5d2ec1951f9a35fcc8ddae3c46e124838a485f23
Tom Roman <tom@tomroman.co.uk>
2021-06-20 17:23:41 +0100

    Create Bank utilities menu

--- src/main/java/network/palace/core/economy/BankMenu.java
+++ src/main/java/network/palace/core/economy/BankMenu.java
@@ -11,11 +11,10 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class BankMenu {
-    private CPlayer player;
+    private final CPlayer player;
 
     public BankMenu(CPlayer player) {
         this.player = player;
@@ -31,6 +30,23 @@ public class BankMenu {
             p.closeInventory();
             openBalance(p);
         })));
+
+        ItemStack utilMenu = new ItemStack(Material.GOLD_RECORD);
+        ItemMeta utilMeta = utilMenu.getItemMeta();
+        utilMeta.setDisplayName(ChatColor.GREEN + "Banking Utilities");
+        List<String> utilLore = new ArrayList<>();
+        utilLore.add(ChatColor.LIGHT_PURPLE + "In this menu you can manage all aspects of");
+        utilLore.add(ChatColor.LIGHT_PURPLE + "your bank account, such as:");
+        utilLore.add(ChatColor.GOLD + "• Trading");
+        utilLore.add(ChatColor.GOLD + "• Recent Spending");
+        utilLore.add(ChatColor.GOLD + "• Leaderboards");
+        utilMeta.setLore(utilLore);
+        utilMenu.setItemMeta(utilMeta);
+        buttons.add(new MenuButton(14, utilMenu, ImmutableMap.of(ClickType.LEFT, p -> {
+            p.closeInventory();
+            openUtilsMenu();
+        })));
+
         Menu inv = new Menu(27, "Bank Menu", player, buttons);
         inv.open();
     }
@@ -49,8 +65,64 @@ public class BankMenu {
         adventureCoins.setItemMeta(advMeta);
         buttons.add(new MenuButton(12, adventureCoins));
 
+        ItemStack backButton = new ItemStack(Material.STICK);
+        ItemMeta backMeta = backButton.getItemMeta();
+        backMeta.setDisplayName(ChatColor.RED + "Go Back");
+        backButton.setItemMeta(backMeta);
+
+        buttons.add(new MenuButton(24, backButton, ImmutableMap.of(ClickType.LEFT, user -> {
+            user.closeInventory();
+            openMenu();
+        })));
+
         Menu inv = new Menu(27, "Bank Balances", p, buttons);
         inv.open();
     }
 
+    private void openUtilsMenu() {
+        List<MenuButton> buttons = new ArrayList<>();
+
+        ItemStack tradingMenu = new ItemStack(Material.SKULL_ITEM);
+        ItemMeta tradingMeta = tradingMenu.getItemMeta();
+        tradingMeta.setDisplayName(ChatColor.GREEN + "Trading Menu");
+        tradingMenu.setItemMeta(tradingMeta);
+
+        ItemStack transactionsMenu = new ItemStack(Material.CLAY_BRICK);
+        ItemMeta transacationsMeta = transactionsMenu.getItemMeta();
+        transacationsMeta.setDisplayName(ChatColor.GREEN + "Transactions Menu");
+        transactionsMenu.setItemMeta(transacationsMeta);
+
+        ItemStack leaderboardMenu = new ItemStack(Material.ARROW);
+        ItemMeta leaderboardMeta = leaderboardMenu.getItemMeta();
+        leaderboardMeta.setDisplayName(ChatColor.GREEN + "Leaderboards Menu");
+        leaderboardMenu.setItemMeta(leaderboardMeta);
+
+        buttons.add(new MenuButton(11, tradingMenu, ImmutableMap.of(ClickType.LEFT, p -> {
+            p.closeInventory();
+            //todo
+        })));
+
+        buttons.add(new MenuButton(13, transactionsMenu, ImmutableMap.of(ClickType.LEFT, p -> {
+            p.closeInventory();
+            //todo
+        })));
+
+        buttons.add(new MenuButton(15, leaderboardMenu, ImmutableMap.of(ClickType.LEFT, p -> {
+            p.closeInventory();
+            //todo
+        })));
+
+        ItemStack backButton = new ItemStack(Material.STICK);
+        ItemMeta backMeta = backButton.getItemMeta();
+        backMeta.setDisplayName(ChatColor.RED + "Go Back");
+        backButton.setItemMeta(backMeta);
+
+        buttons.add(new MenuButton(24, backButton, ImmutableMap.of(ClickType.LEFT, user -> {
+            user.closeInventory();
+            openMenu();
+        })));
+
+        Menu inv = new Menu(27, "Bank Utilities", player, buttons);
+        inv.open();
+    }
 }
